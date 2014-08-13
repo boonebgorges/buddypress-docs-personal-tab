b757838e5c793e2db3b0d7095435608c9280f439
Boone B Gorges <boonebgorges@gmail.com>
2014-08-13 09:45:21 -0400

    Better type hiding

--- bpdpt.js
+++ bpdpt.js
@@ -2,5 +2,6 @@
 	$( document ).ready( function() {
 		$( 'label[for="new-folder-type"]' ).hide();
 		$( 'input[name="new-folder-type"]' ).hide();
+		$( 'select[name="new-folder-type"]' ).hide();
 	} );
 } )( jQuery )
